bef068640826fbdee4e9535926c232bfe16a19a6
Jordan Velkovski <jocka@Jordans-MacBook-Pro.local>
2022-08-03 13:47:24 +0200

    Added one more controller and model

--- database/migrations/2022_08_03_113159_create_employees_table.php
+++ database/migrations/2022_08_03_113159_create_employees_table.php
@@ -13,11 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname')->nullable();
-            $table->unsignedInteger('year');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('companies');
+        Schema::dropIfExists('employees');
     }
 };
